0be9cef2ec58bab0537f75afb8f874684bb2ce4b
FourierTransformer <FourierTransformer@users.noreply.github.com>
2025-04-20 10:15:02 -0500

    now does more exact float representations in tests

--- spec/toml-test.lua
+++ spec/toml-test.lua
@@ -1,9 +1,37 @@
 local cjson = require("cjson")
 local tinytoml = require("tinytoml")
 
+local to_inf_and_beyound = {
+   ["inf"] = true,
+   ["-inf"] = true,
+   ["nan"] = true,
+   ["-nan"] = true,
+}
+
+
+local function floatToString(x)
+
+
+   if to_inf_and_beyound[tostring(x)] then
+      return tostring(x)
+   end
+   for precision = 15, 17 do
+
+      local s = ('%%.%dg'):format(precision):format(x)
+
+      if tonumber(s) == x then
+         return s
+      end
+   end
+end
+
 
 local assign_value_function = function(value, value_type)
-   return { ["value"] = tostring(value), ["type"] = value_type }
+   if value_type == "float" then
+      return { ["value"] = floatToString(value), ["type"] = value_type }
+   else
+      return { ["value"] = tostring(value), ["type"] = value_type }
+   end
 end
 
 print(cjson.encode(tinytoml.parse(io.read("*a"), { load_from_string = true, assign_value_function = assign_value_function })))
